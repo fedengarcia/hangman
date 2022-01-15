5506c0de1507823557ceebb4694c560b14f26fe0
fedengarcia <fedengarcia11@hotmail.com>
2022-01-15 11:06:35 -0300

    Create Router.js

---
+++ src/Components/Router/Router.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import DialogContainer from '../../../../Coder-House/shoes-garcia/src/components/DialogContainer/DialogContainer';
+import GameContainer from '../GameContainer/GameContainer';
+
+
+const Router = () => {
+
+
+    return(
+        <BrowserRouter>
+        <Switch>
+            <Route exact path='/'>
+                <GameContainer/>
+            </Route>
+
+            <Route path='dialog/:dialogType'>
+                <DialogContainer/>
+            </Route>
+        </Switch>
+        </BrowserRouter>
+    )
+}
