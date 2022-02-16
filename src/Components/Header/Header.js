062fe14f4d392b7dfb6068d92edf7f60800884fa
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-02-16 16:09:52 +0100

    ArrowBack Functionality

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,17 +2,26 @@ import React from 'react';
 import {makeStyles} from '@material-ui/core';
 import { HeaderStyle } from './HeaderStyle';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-
+import { useNavigate } from 'react-router-dom';
 
 const useStyle = makeStyles((theme) => HeaderStyle(theme))
 
 export default function Header ({gameTitle}) {
     const classes = useStyle();
+    const navigate = useNavigate();
+
+    const onClickArrowBack = () => {
+        navigate('/')
+    }
 
     return (
     <header className={classes.headerContainer}>
         <div className={classes.arrowBackIconContainer}>
-            <ArrowBackIcon sx={{ fontSize: 50 }} className={classes.arrowBackIcon}/>
+            <ArrowBackIcon 
+            sx={{ fontSize: 50 }}
+            className={classes.arrowBackIcon}
+            onClick={onClickArrowBack}
+            />
         </div>
         <h1>{gameTitle}</h1>
     </header>)
