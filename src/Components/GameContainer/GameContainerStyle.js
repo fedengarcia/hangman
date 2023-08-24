fc87e5a3bea083e82bc7d34f3fbc33d992395da5
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-08-24 20:11:37 +0200

    update home

--- src/Components/GameContainer/GameContainerStyle.js
+++ src/Components/GameContainer/GameContainerStyle.js
@@ -14,30 +14,34 @@ export const GameContainerStyle = (theme) => ({
         backgroundSize: '100% 100%'
     },
     games:{
-        // boxShadow: 'rgba(3, 102, 214, 0.3) 0px 0px 0px 3px;',
         boxShadow: 'rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;',
-
-        backgroundColor: '#ffffff',
+        backgroundColor: '#ffffff8f',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: '1em',
         transition: 'box-shadow 0.1s ease-in',
+        color: '#bd784e',
         '& > h1':{
-            padding: '2em 4em',
+            padding: '1em 2em',
+            fontFamily: 'Handjet',
+            fontSize: '60px',
         },
         '&:hover': {
             boxShadow: 'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;',
-
+            // backgroundColor: '#ffffff',
         }
     },
     button:{
+        fontFamily: 'Handjet',
+        fontWeight: 'bold',
+        fontSize: '1.2vw',
         margin: '0 2em 2em 2em',
         width: '10em',
         '&:hover':{
-            color:'red',
-            opacity: '0.9',
+            color:'#aacc39',
+            opacity: '1',
         }
     },
     linkStyle:{
