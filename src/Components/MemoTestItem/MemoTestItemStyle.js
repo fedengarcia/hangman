34b482ec106867574b3597ecfbeef64dc93f9930
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-08-24 21:54:38 +0200

    memotest styles

--- src/Components/MemoTestItem/MemoTestItemStyle.js
+++ src/Components/MemoTestItem/MemoTestItemStyle.js
@@ -7,6 +7,12 @@ export const MemoTestItemStyle = (theme) => ({
         overflow: 'hidden',
         width:'60%',
         height: '100%',
+        borderRadius: '8px',
+        boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;',
+
+        '&: hover': {
+            boxShadow: 'rgba(255, 255, 255, 0.07) 0px 1px 1px, rgba(255, 255, 255, 0.07) 0px 2px 2px, rgba(255, 255, 255, 0.07) 0px 4px 4px, rgba(255, 255, 255, 0.07) 0px 8px 8px, rgba(255, 255, 255, 0.07) 0px 16px 16px;',
+        }
     },
     memoItemFront:{
         overflow: 'hidden',
